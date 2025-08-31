dc8da6b19f8554b99fdeee32270fb6ea0b0172ac
Your Name <alabiolumide@gmail.com>
2025-08-31 08:57:48 -0700

    product

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -2,14 +2,14 @@ const productModel = require("../Models/product.model")
 
 
 getAllProducts = (req, res) => {
-  Product.find()
+  productModel.find()
     .then((products) => res.json(products))
     .catch((err) => res.status(500).json({ error: err.message }));
 };
 
 
 getProductById = (req, res) => {
-  Product.findOne({ id: req.params.id })
+  productModel.findOne({ id: req.params.id })
     .then((product) => {
       if (!product) return res.status(404).json({ error: "Product not found" });
       res.json(product);
@@ -20,14 +20,14 @@ getProductById = (req, res) => {
 
 searchProducts = (req, res) => {
   const query = req.query.q;
-  Product.find({ name: new RegExp(query, "i") })
+  productModel.find({ name: new RegExp(query, "i") })
     .then((products) => res.json(products))
     .catch((err) => res.status(500).json({ error: err.message }));
 };
 
 
 getByCategory = (req, res) => {
-  Product.find({ category: req.params.cat })
+  productModel.find({ category: req.params.cat })
     .then((products) => res.json(products))
     .catch((err) => res.status(500).json({ error: err.message }));
 }
